366b2a5e75f33e802ca36c63dd2a268343649e69
sandykr29 <sandeepkumar29774@yahoo.com>
2024-03-12 20:49:38 +0530

    main

--- gemini/src/App.js
+++ gemini/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import { Main } from './components/Main/Main';
 import { Sidebar } from './components/Sidebar/Sidebar';
 
 function App() {
   return (
-    <div className="App">
+    <>
      <Sidebar/>
-    </div>
+     <Main/>
+    </>
   );
 }
 
